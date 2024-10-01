a069c9bc0aed375ba7caf0603b83fa902442319b
Ngawang Ghongwatsang <nghongwatsang@gmail.com>
2024-10-01 17:33:04 -0400

    first 2 parking spots finally showing up on map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,7 +1,7 @@
 import Mapbox, {Camera, LocationPuck, MapView, ShapeSource, SymbolLayer, Images} from "@rnmapbox/maps";
 import { isEnabled } from "react-native/Libraries/Performance/Systrace";
 import {featureCollection, point } from '@turf/helpers';
-import pin from '../assets/favicon.png';
+import pin from '../assets/car.jpeg';
 
 
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_KEY || '');
@@ -13,12 +13,12 @@ export default function Map(){
         <LocationPuck puckBearingEnabled  puckBearing = 'heading' pulsing={{isEnabled : true}}/>
         <ShapeSource 
             id = "car" 
-            shape = {featureCollection( [ point([42.729990, -73.677518]) ,  point([42.729940, -73.677534]) ] )}> 
+            shape = {featureCollection( [ point([-73.677518,42.729990]) ,  point([-73.677534, 42.729940]) ] )}> 
             <SymbolLayer
                 id="spots-icons"
                 style={{
-                    iconImage: '{pin}', // use the curly braces around the pin key to reference it properly
-                    iconSize: 1.0,      // you can adjust this as needed
+                    iconImage: 'pin',
+
                 }}
             />
             <Images images={{ pin: pin }} /> 

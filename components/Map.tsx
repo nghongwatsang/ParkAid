0b7c585a9cce0b7e7b5c002dcfd3c1dce8762967
Ngawang Ghongwatsang <nghongwatsang@gmail.com>
2024-10-21 21:57:17 -0400

    start of developing routes to spots

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,20 +1,23 @@
-import Mapbox, {Camera, LocationPuck, MapView, ShapeSource, SymbolLayer, Images, CircleLayer} from "@rnmapbox/maps";
+import Mapbox, {Camera, LocationPuck, MapView, ShapeSource, SymbolLayer, Images, CircleLayer, LineLayer} from "@rnmapbox/maps";
 import { isEnabled } from "react-native/Libraries/Performance/Systrace";
 import {featureCollection, point } from '@turf/helpers';
 import pin from '../assets/car_green.png';
 import cars from '../data/spots.json'
+import routeResponse from '../data/routes.json'
 
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_KEY || '');
 
 export default function Map(){
     const spots = cars.map((cars) => point([cars.latitude,cars.longitude]))
-
+    const routeData = routeResponse.routes[0].geometry.coordinates;
     return (
     <MapView style = {{flex:1}} >
         <Camera followZoomLevel={10} followUserLocation />
         <LocationPuck puckBearingEnabled  puckBearing = 'heading' pulsing={{isEnabled : true}}/>
         
         <ShapeSource id = "car" cluster shape = {featureCollection(spots)}  onPress={(e) => console.log(JSON.stringify(e, null, 2))}>
+            
+            {/* Number Denoting how many are in a cluster */}
             <SymbolLayer
                 id = "clusters_count"
                 style={{
@@ -26,7 +29,7 @@ export default function Map(){
   
                 }}
             />
-
+            {/* the cicle that appears when spot icons are clustered*/}
             <CircleLayer
                 id = 'cluster'
                 belowLayerID="clusters_count"
@@ -40,7 +43,7 @@ export default function Map(){
                     circleStrokeColor: 'white',
                 }}
             />
-
+            {/* Symbol denoting a open spot */}
             <SymbolLayer
                 id= "spots-icons"
                 filter = {['!', ['has', 'point_count']]}
@@ -52,8 +55,35 @@ export default function Map(){
                 }}
             />
             <Images images={{ pin: pin }} /> 
-        
         </ShapeSource>
+
+        {routeData &&  (
+            <ShapeSource
+                id = 'routeSource'
+                lineMetrics
+                shape ={{
+                    properties: {},
+                    type: 'Feature',
+                    geometry: {
+                        type: 'LineString',
+                        coordinates: routeData
+                    },
+                }}>
+                <LineLayer
+                    id = 'lineLayer'
+                    style={{
+                        lineColor: '#33ccff',
+                        lineCap: 'round',
+                        lineJoin: 'round',
+                        lineWidth: 7, 
+                    }}
+                />
+
+            </ShapeSource>
+
+
+
+        )}
 
     </MapView>
     ); 

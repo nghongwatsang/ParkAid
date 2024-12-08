f77a3fd6e69efbc2f0d69bcc8dc7842a34d840ac
Ngawang Ghongwatsang <nghongwatsang@gmail.com>
2024-12-08 14:54:09 -0500

    Continuing the proces sof implementing the bottom overlay

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import SpotProvider from '~/provider/SpotProvider';
 
 export default function Layout() {
   return (
+    <GestureHandlerRootView style = {{flex: 1}}> 
       <SpotProvider>
 
       <Stack />
       
       </SpotProvider>
+      </GestureHandlerRootView>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,23 @@
 import { Stack, Link } from 'expo-router';
-
+import BottomSheet from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheet/BottomSheet';
 import Map from '~/components/Map'
 
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
+import bottomSheet from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheet';
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Home', headerShown:false }} />
       <Map/>
+      <BottomSheet
+        ref = {bottomSheetRef}
+        onChange = {handleSheetChanges}
+        >
+        
+      </BottomSheet>
     </>
   );
 }

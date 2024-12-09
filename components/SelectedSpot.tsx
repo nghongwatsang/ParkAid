3d75bd27701923f1b67242ef16942c371c143135
Ngawang Ghongwatsang <nghongwatsang@gmail.com>
2024-12-09 17:01:29 -0500

    adding more to the overlay

--- components/SelectedSpot.tsx
+++ components/SelectedSpot.tsx
@@ -1,8 +1,24 @@
 import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import { useEffect, useRef } from "react";
 import { Text } from "react-native-reanimated/lib/typescript/Animated";
+import { useSpot } from "~/provider/SpotProvider";
 export default function SelectedSpot (){
+
+    const { selectedSpot} = useSpot();
+    const bottomSheet = useRef<BottomSheet>(null);
+
+    useEffect(() => {
+      if ( selectedSpot){
+           bottomSheet.current?.expand();
+      }
+    }, [selectedSpot]);
+
     return(
-        <BottomSheet  index = {-1} snapPoints={[150, '45%']} enablePanDownToClose>
+        <BottomSheet  
+        index = {-1} 
+        snapPoints={[150, '45%']} 
+        enableDynamicSizing
+        enablePanDownToClose>
         <BottomSheetView style = {{}}>
           <Text> OVERLAY FEATURE</Text>
         </BottomSheetView>
